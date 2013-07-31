eb20a941f63868e9a7e265d1afb2967af684d9fb
Emily Purcell <someone@someplace.com>
2013-07-30 23:39:01 -0600

    Created base html file using Twitter Bootstrap

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -1,4 +1,5 @@
 # Django settings for blog project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -111,6 +112,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    os.path.join(DIRNAME, "templates")
 )
 
 INSTALLED_APPS = (
