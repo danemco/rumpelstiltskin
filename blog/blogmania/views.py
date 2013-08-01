1b53da534262dc6ff3401f25c6e43b6473328214
Emily Purcell <someone@someplace.com>
2013-07-31 23:12:37 -0600

    Blog views and base index html

--- blog/blogmania/views.py
+++ blog/blogmania/views.py
@@ -1,1 +1,19 @@
 # Create your views here.
+from django.utils import timezone
+from django.views import generic
+
+from blogmania.models import Post
+
+class IndexView(generic.ListView):
+    template_name = "blogmania/index.html"
+    context_object_name = 'blog_posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            pub_date__lte=timezone.now()
+        )
+
+class DetailView(generic.DetailView):
+    template_name = 'blogmania/detail.html'
+    model = Post
+
