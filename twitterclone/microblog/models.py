2af9759df5748089c1fdc0407cf2a54b9f7fb3be
Emily Purcell <someone@someplace.com>
2013-08-02 08:08:57 -0600

    Completed models. Edited urls.py to handle static files for uploads.

--- twitterclone/microblog/models.py
+++ twitterclone/microblog/models.py
@@ -7,8 +7,17 @@ class Profile(models.Model):
     user = models.ForeignKey(Users)
     bio  = models.TextField('About me')
     # Where I left off
-    # picture = models.
-
+    picture = models.ImageField('Profile Image', upload_to='pictures/%Y/%m/%d', blank="True")
+    following = models.ManyToMany('self')
 
     def __unicode__(self):
         return self.user.username
+
+class Post(models.Model):
+    profile = models.ForeignKey(Profile)
+    message  = models.CharField(max_length=140)
+    pub_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.message
+
