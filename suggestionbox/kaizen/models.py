9f508a6657495541fb348bffc1fbd6294643dd9f
Dan Purcell <dpurcell@dpurcell-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-09-24 21:39:58 -0600

    Model work for Kaizen app

--- suggestionbox/kaizen/models.py
+++ suggestionbox/kaizen/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Status(models.Model):
+    status = models.CharField(max_length=25)
+
+    def __unicode__(self):
+        return self.status
+
+class Idea(models.Model):
+    user = models.ForienKey(User)
+    title = models.CharField(max_length=100)
+    body = models.TextField('suggestion')
+    status = models.ForeignKey(Status)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return title
+
+class Comment(models.Model):
+    comment = models.TextField()
+    idea = models.ForeignKey(Idea)
+
+    def __unicode__(self):
+        return comment
