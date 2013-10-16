b59b273606a9299bae30e71f0ca13f31011eade8
Dan Purcell <dpurcell@dpurcell-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-10-15 23:57:05 -0600

    Created some models.

---
+++ ticktock/timeclock/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
+class Record(models.Model):
+    user = models.ForeignKey(User)
+    project = models.ForeignKey(Project)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    message = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return start_time
